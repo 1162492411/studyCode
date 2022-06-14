aeffe755c4fc7bcbc27d4987b819e92abec93edd
zyg <yanguang.zhang@aihuishou.com>
2022-06-14 16:56:10 +0800

    [protobuf - 示例]添加json与pb对比代码

--- parent/protobuf/src/main/java/com/study/protobuf/demo/Demo.java
+++ parent/protobuf/src/main/java/com/study/protobuf/demo/Demo.java
@@ -14,6 +14,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
 
+import java.io.File;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.nio.charset.StandardCharsets;
@@ -93,10 +94,10 @@ public class Demo {
 
     /**
      * 示例 - 从文件读取json对象转换为pb对象,计算提升率
-     * @param fileName
+     * @param file
      */
-    public static void parseFromJsonToPbDemo(String fileName) {
-        String readStr = ResourceUtil.readStr(fileName, StandardCharsets.UTF_8);
+    public static void parseFromJsonToPbDemo(File file) {
+        String readStr = ResourceUtil.readStr(file.getPath(), StandardCharsets.UTF_8);
         List<ProductSeriesGroupRes> productSeriesGroupResList = stringToObjectListWithJson(readStr);
         int jsonTotalLength = JSON.toJSONString(productSeriesGroupResList).getBytes(StandardCharsets.UTF_8).length;
         log.info("【对象大小-json】json方式总长度为{}",jsonTotalLength);
@@ -145,7 +146,7 @@ public class Demo {
         BigDecimal top = new BigDecimal((jsonTotalLength - pbTotalLength));
         BigDecimal bottom = new BigDecimal(jsonTotalLength);
         BigDecimal effect = top.divide(bottom, 2, RoundingMode.FLOOR).multiply(new BigDecimal(100));
-        log.info("【{}提升率】pb相比json缩小了{}%",fileName,effect);
+        log.info("【{}提升率】pb相比json缩小了{}%",file.getName(),effect);
     }
 
     @SneakyThrows
@@ -154,8 +155,44 @@ public class Demo {
         //build : protoc --proto_path=IMPORT_PATH --java_out=DST_DIR path/to/file.proto
         //build该module,目的是使json文件编译到classes中
         FileUtil.loopFiles("originJson").forEach(file -> {
-            parseFromJsonToPbDemo(file.getPath());
+            parseFromJsonToPbDemo(file);
         });
+        //运行结果
+        /**
+         * 16:54:37.173 [main] INFO com.study.protobuf.demo.Demo - 【对象大小-json】json方式总长度为19947
+         * 16:54:37.240 [main] INFO com.study.protobuf.demo.Demo - 【对象大小】pb方式的对象长度为14727
+         * 16:54:37.240 [main] INFO com.study.protobuf.demo.Demo - 【thinkPad.json提升率】pb相比json缩小了26.00%
+         * 16:54:37.265 [main] INFO com.study.protobuf.demo.Demo - 【对象大小-json】json方式总长度为44484
+         * 16:54:37.271 [main] INFO com.study.protobuf.demo.Demo - 【对象大小】pb方式的对象长度为29531
+         * 16:54:37.272 [main] INFO com.study.protobuf.demo.Demo - 【huaShuo.json提升率】pb相比json缩小了33.00%
+         * 16:54:37.273 [main] INFO com.study.protobuf.demo.Demo - 【对象大小-json】json方式总长度为181
+         * 16:54:37.273 [main] INFO com.study.protobuf.demo.Demo - 【对象大小】pb方式的对象长度为83
+         * 16:54:37.273 [main] INFO com.study.protobuf.demo.Demo - 【yota.json提升率】pb相比json缩小了54.00%
+         * 16:54:37.282 [main] INFO com.study.protobuf.demo.Demo - 【对象大小-json】json方式总长度为16844
+         * 16:54:37.284 [main] INFO com.study.protobuf.demo.Demo - 【对象大小】pb方式的对象长度为10922
+         * 16:54:37.284 [main] INFO com.study.protobuf.demo.Demo - 【sanXing.json提升率】pb相比json缩小了35.00%
+         * 16:54:37.290 [main] INFO com.study.protobuf.demo.Demo - 【对象大小-json】json方式总长度为8330
+         * 16:54:37.291 [main] INFO com.study.protobuf.demo.Demo - 【对象大小】pb方式的对象长度为4726
+         * 16:54:37.291 [main] INFO com.study.protobuf.demo.Demo - 【jinLi.json提升率】pb相比json缩小了43.00%
+         * 16:54:37.300 [main] INFO com.study.protobuf.demo.Demo - 【对象大小-json】json方式总长度为11159
+         * 16:54:37.303 [main] INFO com.study.protobuf.demo.Demo - 【对象大小】pb方式的对象长度为6228
+         * 16:54:37.304 [main] INFO com.study.protobuf.demo.Demo - 【vivo.json提升率】pb相比json缩小了44.00%
+         * 16:54:37.309 [main] INFO com.study.protobuf.demo.Demo - 【对象大小-json】json方式总长度为15457
+         * 16:54:37.310 [main] INFO com.study.protobuf.demo.Demo - 【对象大小】pb方式的对象长度为9902
+         * 16:54:37.311 [main] INFO com.study.protobuf.demo.Demo - 【dongZhi.json提升率】pb相比json缩小了35.00%
+         * 16:54:37.325 [main] INFO com.study.protobuf.demo.Demo - 【对象大小-json】json方式总长度为38792
+         * 16:54:37.329 [main] INFO com.study.protobuf.demo.Demo - 【对象大小】pb方式的对象长度为28138
+         * 16:54:37.329 [main] INFO com.study.protobuf.demo.Demo - 【daiEr.json提升率】pb相比json缩小了27.00%
+         * 16:54:37.334 [main] INFO com.study.protobuf.demo.Demo - 【对象大小-json】json方式总长度为10302
+         * 16:54:37.335 [main] INFO com.study.protobuf.demo.Demo - 【对象大小】pb方式的对象长度为5720
+         * 16:54:37.335 [main] INFO com.study.protobuf.demo.Demo - 【oppo.json提升率】pb相比json缩小了44.00%
+         * 16:54:37.343 [main] INFO com.study.protobuf.demo.Demo - 【对象大小-json】json方式总长度为39692
+         * 16:54:37.344 [main] INFO com.study.protobuf.demo.Demo - 【对象大小】pb方式的对象长度为28489
+         * 16:54:37.344 [main] INFO com.study.protobuf.demo.Demo - 【lianXiang.json提升率】pb相比json缩小了28.00%
+         * 16:54:37.347 [main] INFO com.study.protobuf.demo.Demo - 【对象大小-json】json方式总长度为13955
+         * 16:54:37.348 [main] INFO com.study.protobuf.demo.Demo - 【对象大小】pb方式的对象长度为8437
+         * 16:54:37.348 [main] INFO com.study.protobuf.demo.Demo - 【huaWei.json提升率】pb相比json缩小了39.00%
+         */
     }
 
 }
